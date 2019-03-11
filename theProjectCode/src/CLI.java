19a09121545fb53080f6b945b3542d3bfcc52489
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2019-03-11 17:27:12 +0000

    CLI

--- theProjectCode/src/CLI.java
+++ theProjectCode/src/CLI.java
@@ -1,4 +1,87 @@
+import java.util.Scanner;
 
-public class CLI {
+public class CLI
+{
+	private MenuChoice menu;
+	
+	public static void main(String[] args)
+	{
+		CLI commandLineMenu = new CLI();
+		commandLineMenu.initialise();
+		while(true)
+		{
+			commandLineMenu.process();
+		}
+	}
 
+	public void initialise()
+	{
+		menu = new MenuChoice();
+	}
+	
+	public void process()
+	{
+		displayMenu();
+		
+		Scanner menuScanner = new Scanner(System.in);
+		String choice = menuScanner.nextLine();
+		int menuOption = Integer.parseInt(choice);
+	
+		switch(menuOption)
+		{		
+			case 1:
+				System.out.println("case 1");
+				//Translate method
+				break;
+			case 2:
+				System.out.println("case 2");
+				//Read method
+				break;
+			case 3:
+				System.out.println("case 3");
+				//Display dictionary method
+				break;
+			case 4:
+				System.out.println("case 4");
+				//Export dictionary method
+				break;
+			case 5:
+				System.out.println("case 5");
+				//Import dictionary method
+				break;
+			case 6:
+				System.out.println("case 6");
+				//Add word dictionary method
+				break;
+			case 7:
+				System.out.println("case 7");
+				//Remove word dictionary method
+				break;
+			case 8:
+				System.out.println("");
+				System.out.println("You have chosen to exit");
+				System.exit(0);
+				break;
+			default:
+				System.out.println("Invalid choice, please re-enter your choice: ");
+				break;
+		}
+	}
+	
+	/**
+	 * Holds information about the menu.
+	 */
+	public void displayMenu()
+	{
+		System.out.println("");
+		System.out.println("Please select one of the options below:");
+		System.out.println("1. Translate Input");
+		System.out.println("2. Translate File");
+		System.out.println("3. Display Dictionary");
+		System.out.println("4. Export Dictionary");
+		System.out.println("5. Import Dictionary");
+		System.out.println("6. Add Word To Dictionary");
+		System.out.println("7. Remove Word From Dictionary");
+		System.out.println("8. Exit Program");
+	}
 }
