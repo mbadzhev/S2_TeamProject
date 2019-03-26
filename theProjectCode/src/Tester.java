849726908d1d70a3b9b032274263d810fda6ee82
bkopplin <bkopplin@dundee.ac.uk>
2019-03-26 10:58:03 +0000

    wrote tests for translateFile

--- theProjectCode/src/Tester.java
+++ theProjectCode/src/Tester.java
@@ -25,7 +25,21 @@ public class Tester {
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		testTrans.translateFile("english.txt", "german.txt", "en-de");
-
+		// translateFile()
+		System.out.println("translate Rotkappchen");
+		testTrans.translateFile("Rotkäppchen.txt", "Rotkäppchen_en.txt", "de-en");
+		
+		System.out.println("translate rock_climbing_explained");
+		testTrans.translateFile("rock_climbing_explained.txt", "rock_climbing_explained_de.txt", "en-de");
+		
+		System.out.println("translate not existing file");
+		testTrans.translateFile("not_existing_file.txt", "output.txt", "en-de");
+		
+		System.out.println("translateFile and not specifying an output file");
+		testTrans.translateFile("rock_climbing_explained.txt", "", "en-de");
+		
+		System.out.println("translateFile with wrong direction");
+		testTrans.translateFile("rock_climbing_explained.txt", "rock_climbing_explained_de.txt", "german - english");
+		// TODO how to handle directions that do not exist yet? Shall we asks to create a new dictionary?
 	}
 }
